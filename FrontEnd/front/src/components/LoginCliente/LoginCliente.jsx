f48458932cec58dd69625a4dcd63c6202f353dc7
Ney Perea <nperea75@gmail.com>
2022-11-03 13:12:20 -0500

    se ajusta layout de logincliente

--- FrontEnd/front/src/components/LoginCliente/LoginCliente.jsx
+++ FrontEnd/front/src/components/LoginCliente/LoginCliente.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import logincliente from '../Image/logincliente.png';
+import "../LoginCliente/LoginCliente.css"
 
 
 export const LoginCliente = () => {
   return (
-    <div className='container'>
+    <div className='container-lc'>
         <div className='container_login'>
             <img className='loginCliente' src={logincliente}></img>
         </div>
-        <div className='login'>
+        <div className='login-lc'>
             <input type="text" placeholder='Correo Electronico' />
             <input type="text" placeholder='Ingresar Contraseña' />
             <Link to={'/homecliente'}><button type='submit'>
