ff4285be47ac1147b0fbeb6b19fcf7b4a6a73661
Ney Perea <nperea75@gmail.com>
2022-11-03 08:15:14 -0500

    se ajusta layout de ingresar cliente

--- FrontEnd/front/src/components/CrearingresarCliente/CrearIngresarCliente.jsx
+++ FrontEnd/front/src/components/CrearingresarCliente/CrearIngresarCliente.jsx
@@ -2,24 +2,20 @@ import React from 'react'
 import ClientesLogin from '../Image/ClientesLogin.png';
 import '../CrearingresarCliente/CrearIngresarCliente.css';
 import { Link } from 'react-router-dom';
-import '../CrearingresarCliente/CrearIngresarCliente.css';
 
 export const CrearIngresarClinete = () => {
   return (
-    <div className='container'>
-    <div className='container_crear'>
-        <div className='crear'>
-            <img className='Tendero' src={ClientesLogin} alt="img"></img>
-            <Link to={'/crearusuario'}>
-                <button className='button_products'>Crear Cuenta</button>
-            </Link>
+    <div className='main_container-cic'>
+        <div className='container_img'>
+            <img className='cliente-cic' src={ClientesLogin} alt="img"></img>
+        </div>  
+
+        <div className='container_btn-cic'>
+            <Link to={'/crearusuario'}> <button className='button_products'>Crear Cuenta</button> </Link>
+            <Link to={'/logincliente'}> <button className='button_products'>Ingresar</button> </Link>
         </div>
-        <div className='Ingresar'>
-            <Link to={'/logincliente'}>
-                <button className='button_products'>Ingresar</button>
-            </Link>
-        </div>
+        
     </div>
-</div>
+
   )
 }
